62d9c26a975c79a9755a4bce0d40377691430141
kouakouetienne <kouakouetienne03@gmail.com>
2026-02-01 02:08:02 +0000

    mise à jour analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { SiteFooter } from "@/components/site-footer";
 import { WhatsAppFloat } from "@/components/whatsapp-float";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
             <div className="flex-1">{children}</div>
             <SiteFooter />
             <WhatsAppFloat />
+            <Analytics />
           </div>
         </ThemeProvider>
       </body>
